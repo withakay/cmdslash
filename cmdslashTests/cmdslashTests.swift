683e4cac774a2cd102fb34d97ef3c40acae26572
withakay <jack@fader.co.uk>
2025-08-18 18:37:38 +0100

    Initial Commit

---
+++ cmdslashTests/cmdslashTests.swift
@@ -0,0 +1,17 @@
+//
+//  cmdslashTests.swift
+//  cmdslashTests
+//
+//  Created by Jack Rutherford on 18/08/2025.
+//
+
+import Testing
+@testable import cmdslash
+
+struct cmdslashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
